1cc3ab3b616389bfc0fc0245a164ade6f8485365
Brayan Perez <brayanperez@Brayans-Air.lan>
2023-03-16 22:39:43 -0700

    final commit

--- script.js
+++ script.js
@@ -17,30 +17,68 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  const mainTitle = document.getElementById('main-title');
+  mainTitle.textContent = "New Title";
+  
 
   // Part 2
-
+  const body = document.querySelector('body');
+  body.style.backgroundColor = "blue";
+  
 
   // Part 3
-
+  const favoriteThings = document.getElementById('favorite-things');
+  favoriteThings.lastElementChild.remove();
+  
 
   // Part 4
-
+  const specialTitles = document.querySelectorAll('.special-title');
+  specialTitles.forEach(title => {
+    title.style.fontSize = "2rem";
+  });
+  
 
   // Part 5
-
+  const pastRaces = document.getElementById('past-races');
+  const chicago = pastRaces.querySelector('li:last-child');
+  chicago.remove();
+  
 
   // Part 6
-
+  const newRace = document.createElement('li');
+  newRace.textContent = "New York City";
+  pastRaces.appendChild(newRace);
+  
 
   // Part 7
-
+  const blogContainer = document.querySelector('.main');
+  const newBlogPost = document.createElement('div');
+  newBlogPost.classList.add('blog-post');
+  const newHeading = document.createElement('h2');
+  newHeading.textContent = "New York City";
+  const newContent = document.createElement('p');
+  newContent.textContent = "Some text about New York City";
+  newBlogPost.appendChild(newHeading);
+  newBlogPost.appendChild(newContent);
+  blogContainer.appendChild(newBlogPost);
+  
 
   // Part 8
-
+  const quoteTitle = document.getElementById('quote-title');
+  quoteTitle.addEventListener('click', randomQuote);
+  
 
   // Part 9
+  const blogPosts = document.querySelectorAll('.blog-post');
+  blogPosts.forEach(post => {
+    post.addEventListener('mouseout', () => {
+      post.classList.toggle('purple');
+    });
+    post.addEventListener('mouseenter', () => {
+      post.classList.toggle('red');
+    });
+  });
+  
 
 
 
